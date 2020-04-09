abc34d5f175c624ffc944e4d746e0261ede44b0a
madde1 <maddeswish@gmail.com>
2020-04-09 13:46:30 +0200

    Updates

---
+++ precache-manifest.5dd8e3f9d2715e666158b5e3d4fcb609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2c0b4ecae874bcbd7b8b3bc3a9e6dd",
+    "url": "/Movie-series-info-page/index.html"
+  },
+  {
+    "revision": "f37026fbbdfd7dd81662",
+    "url": "/Movie-series-info-page/static/css/main.9f526ebb.chunk.css"
+  },
+  {
+    "revision": "a40222f51ee598d63e78",
+    "url": "/Movie-series-info-page/static/js/2.f9468406.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-series-info-page/static/js/2.f9468406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37026fbbdfd7dd81662",
+    "url": "/Movie-series-info-page/static/js/main.a206cb23.chunk.js"
+  },
+  {
+    "revision": "f046acc8664ce3fce8b2",
+    "url": "/Movie-series-info-page/static/js/runtime-main.82d0485f.js"
+  }
+]);
