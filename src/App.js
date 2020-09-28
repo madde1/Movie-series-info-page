7d5fa2ff33a2f786492d37b92ee91937aa4dff1d
madde1 <maddeswish@gmail.com>
2020-09-28 13:35:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import LandingHome from './components/home-page/LandingHome'
-
+import LandingSocialMediaApp from './components/home-social-media-app/LandingSocialMediaApp'
 import Landing from './components/home-movie/Landing'
 import Movie from './components/home-movie/Movie'
 import store from './store'
@@ -19,6 +19,7 @@ class App extends Component{
                     <Navbar />
                     <Route exact path="/" component={LandingHome}/>
                     <Route exact path="/movie-tvseries-search/" component={Landing} />
+                    <Route exact path="/social-media-app/" component={LandingSocialMediaApp} />
                     <Route exact path="/movie/:id" component={Movie} />
                     <Footer />
                 </div>
---
+++ src/components/home-social-media-app/LandingSocialMediaApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class LandingSocialMediaApp extends Component{
+
+    render(){
+        return(
+                <section className="grid-container">
+                    <header>social media </header>
+                <main>
+                    <p>social media</p>
+                 </main>
+                 <footer>All rights reservd</footer>
+                </section>
+        )
+    }
+}
+
+export default LandingSocialMediaApp
